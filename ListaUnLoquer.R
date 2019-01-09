acfb9b2b5acb11a8049c05e75112d6a454474c2b
brolin <brolin108@gmail.com>
2019-01-09 15:56:56 -0500

    Procesamiento de los registros para agrupar por hilos. En proceso, hay que depurar el reemplazo de palabras dentro del subject

--- ListaUnLoquer.R
+++ ListaUnLoquer.R
@@ -1,28 +1,63 @@
 rm(list=ls())
 
-require(Gmisc)
-require(memisc)
-require(ggplot2)
-require(reshape)
+require(tidyverse)
+require(lubridate)
 
-Mensajes <- read.csv2("./Data/ListaUnLoquer.csv", sep=";")
+## Carga los registros desde el sistema de archivo
+Mensajes2019 <- read_csv("./Data/2019/IndiceCorreos.csv", col_names=F)
+colnames(Mensajes2019) <- c("subject","from","to","date","hasattachment","fullcontent")
 
-p <- ggplot(Mensajes, aes(Date)) + geom_bar(stat="bin")
-p
+## Limpia mensajes que no corresponden directamente a mensajes de la lista
+sinIssues <- Mensajes2019[!str_detect(Mensajes2019$subject,"Re: \\[unloquer/AQA\\].*"),] %>% as_tibble
 
-MensajesXDia <- as.data.frame(table(Mensajes$Date))
+## Convierte a formato fecha la columna date
+sinIssues$date <- dmy_hm(sinIssues$date)
 
-colnames(MensajesXDia) <- c("date","n_correos")
-MensajesXDia <- MensajesXDia[-1166,]
-write.csv(MensajesXDia,"./Data/MensajesXDia.csv")
+## Busca correos que SON respuestas de un hilo y obtiene los índices de los registros
+responseString <- "Re: \\[un/loquer\\] "
+threadseed.idx <- which(is.na(sinIssues$subject %>% str_extract(responseString)))
+## Busca correos que NO SON respuestas de un hilo y obtiene los índices de los registros
+threads.idx <- !is.na(sinIssues$subject %>% str_extract(responseString))
 
-MensajesXDia[which(MensajesXDia$n_correos == as.numeric(max(MensajesXDia$n_correos))),]
-MensajesXDia[which(MensajesXDia$n_correos == as.numeric(min(MensajesXDia$n_correos))),]
+## Correos respuesta y conforman un hilo sumarizados por cantidad de apariciones
+uniqueThreads <- sinIssues$subject[threads.idx] %>% str_replace(responseString,"") %>% str_replace_all("\"","") %>% as_tibble %>% mutate(cnt = "") %>% group_by(value) %>% summarise(cnt = n())
+colnames(uniqueThreads) <- c("subject","cnt")
+uniqueThreads$flag <- "*"
 
-MensajesXDia$date <- strptime(MensajesXDia$date,"%Y%m%d")
-template <- seq(fechas[1],fechas[length(fechas)],"day")
-plantilla <- data.frame(template)
+uniqueThreads %>% View
 
-MensajesXDia <- join(plantilla,MensajesXDia, "date")
-MensajesXDia$n_correos[which(is.na(MensajesXDia$n_correos))] <- 0
-write.csv(MensajesXDia,"./Data/MensajesXDia.csv")
+## Correos semilla de un hilo, es posible que no tengan una respuesta, sumarizados por subject
+threadseed <- sinIssues[threadseed.idx, "subject"] %>% mutate(cnt = "") %>% group_by(subject) %>% summarise(cnt = n()) %>% as_tibble
+## Se unen sumatoria de correos respuesta de un hilo y las semillas
+mailThreads <- threadseed %>% left_join(uniqueThreads, by="subject")
+
+mailThreads %>% arrange(desc(cnt.y,cnt.x)) %>% View
+
+## Obtener las fecha y sender del correo semilla y las respuestas del hilo
+mailThreads %>% filter(cnt.x > 1) ## Seed de los que no se puede obtener la fecha direcatmente porque hay varios con el mismo nombre
+mailThreads[-which(mailThreads$cnt.x > 1),]
+
+map(mailThreads$subject, function(s) {})
+
+a <- sinIssues %>% group_by(subject) %>% nest
+
+jsonlite::toJSON(a)
+
+
+sinIssues[threadseed.idx, c("subject","date","from")]
+
+sinIssues[threads.idx,] %>% group_by(subject) %>% str_replace(responseString,"") %>% str_replace_all("\"","") %>% as_tibble
+
+
+
+sinIssues$subject %>% str_replace(responseString,"") %>% as_tibble
+
+sinIssues[is.na(sinIssues$subject %>% str_extract("Re: ")),c("from","subject","date")]
+
+## Algunos cálculos con resúmenes de los datos
+sinIssues %>% group_by(from) %>% summarise(cnt = n()) %>% View
+sinIssues %>% group_by(to) %>% summarise(cnt = n()) %>% View
+
+sinIssues %>% group_by(from, `año` = year(sinIssues$date), mes = month(sinIssues$date)) %>% summarise(cnt = n()) %>% arrange(`año`, mes) %>% View
+
+sinIssues %>% group_by(from, subject) %>% summarise(cnt = n()) %>% View
